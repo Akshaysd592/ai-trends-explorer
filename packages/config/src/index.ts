3346938c061d7004a40b9710c74b0b90bd51d8d7
Akshay Dhobale <109212657+Akshaysd592@users.noreply.github.com>
2026-07-26 11:57:11 +0530

    Revert "feat: initialize Nx monorepo foundation (#6)"
    This reverts commit 5ee2ba45ccc9d7040df89a9efdee65b425b14923.

--- packages/config/src/index.ts
+++ packages/config/src/index.ts
@@ -1,4 +1,1 @@
-export * from './lib/config.module.js';
-export * from './lib/config.service.js';
-export * from './lib/configuration.js';
-export * from './lib/env.schema.js';
+export * from './lib/config.js';
